3f8962b04277f3c01848b3e54ba66f6b02ac5f12
Matt Boubin <boubinmj@miamioh.edu>
2016-03-08 18:00:11 -0500

    sensor verification for UV sensor

--- Midterm/Soil_Moisture/Soil_Moisture.ino
+++ Midterm/Soil_Moisture/Soil_Moisture.ino
@@ -5,15 +5,28 @@ void setup() {
   //Serial.println("LABEL,TIME,X");
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int x = analogRead(A5);
   
-  Serial.print(x);
-  Serial.println(",");
+  if(x > 900){
+    digitalWrite(13, HIGH);    
+  }
+  else if(x > 600){
+    digitalWrite(12, HIGH);    
+  }
+  else if(x > 300){
+    digitalWrite(11, HIGH);    
+  }
+  
   delay(1000);
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
   
 }
