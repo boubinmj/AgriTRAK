2469ea8ea0d37c92be2c37f3a1feeaaced4c2d0d
benjamam <benjamam@miamioh.edu>
2016-03-09 11:57:57 -0500

    water level and temp sensor code

--- Temp_WaterLvl/Temp_WaterLvl.ino
+++ Temp_WaterLvl/Temp_WaterLvl.ino
@@ -1,6 +1,8 @@
 #include <OneWire.h> 
 #define SERIESRESISTOR 470
 #define ETAPE A0
+#define LED_GREEN 12
+#define LED_RED 11
 
 int DS18S20_Pin = 2; //DS18S20 Signal pin on digital 2
 
@@ -9,23 +11,41 @@ OneWire ds(DS18S20_Pin);  // on digital pin 2
 
 void setup(void) {
   Serial.begin(9600);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
 } 
 
 void loop(void) {
+  /*read temperature from Temp Sensor*/
   float temperature = getTemp();
- // Serial.print("Water temp: ");
-  //Serial.println(temperature);
+  Serial.print("Water temp: ");
+  Serial.println(temperature);
+
+  /*If temp is below 15 deg celcius, red led is on. If temp is above 15
+  deg, green led is on*/
+  if(temperature < 15){
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+  }
+  else {
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, HIGH);    
+  }
+
+  /*read voltage value from ETAPE*/
   float sensorValue = analogRead(ETAPE);
-//convert value to resistance
+  //convert value to resistance
   sensorValue = (1023.0/sensorValue) - 1.0;
   sensorValue = SERIESRESISTOR/sensorValue;
 
 
   float voltage = sensorValue * (3.3/1023.0);
-  // print out the value you read:
   Serial.print("eTape: ");
   Serial.println(sensorValue);
+  
   delay(1000); //just here to slow down the output so it is easier to read
+
+
   
 }
 
