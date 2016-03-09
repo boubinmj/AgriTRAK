eba85ad0bebb5e538cd382c6e7490349498a00d5
Matt Boubin <boubinmj@miamioh.edu>
2016-03-09 00:05:20 -0500

    added stuff to midterm project

---
+++ Midterm/Midterm/Midterm.h
@@ -0,0 +1,13 @@
+/********************************
+* Define DIGITAL Pins          
+********************************/
+
+#define LED_RED      	(12)      		//Red LED pin
+#define LED_YELLOW    	(11)       		//Yellow LED pin
+#define LED_GREEN    	(13)       		//Green LED pin
+
+/********************************
+ *Define ANALOG Pins
+********************************/
+
+#define MOISTURE    	(A5)       		//Soil Moisture VIN
